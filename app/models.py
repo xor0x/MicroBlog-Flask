9b9ac9f8d9864e805f4797760cca2b5ebcae1054
xor0x <alex.misyuk1@gmail.com>
2021-12-26 10:40:43 +0200

    first commit

---
+++ app/models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+from app import db
+from app import login
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username= db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password = db.Column(db.String(128))
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
+
+    def __repr__(self):
+        return f"User {self.username}"
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return f"Post {self.content}"
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
